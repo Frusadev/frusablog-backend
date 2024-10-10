8139a491028d0173233ce768ea36715dd61d0ce6
Frusadev <frusadev@gmail.com>
2024-10-10 17:51:57 +0000

    Fix security

--- app/api/endpoints/settings.py
+++ app/api/endpoints/settings.py
@@ -2,13 +2,20 @@ import os
 
 from dotenv import load_dotenv
 
-_ = load_dotenv()
+load_dotenv(os.getcwd() + "/.env")
+
+
+class EnvironmentError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
 
 SECRET_KEY = os.getenv("secret")
 ALGORITHM = os.getenv("algorithm")
 ACCESS_TOKEN_EXPIRATION_TIME = 40  # In days
+
 if not SECRET_KEY:
-    SECRET_KEY = "7a37f743415645aa80efd123ff83b4c9a13010e4e2561024959c421c428eb298"
+    raise EnvironmentError("Could not load SECRET_KEY")
 
 if not ALGORITHM:
-    ALGORITHM = "HS256"
+    raise EnvironmentError("Could not load ALGORITHM")
