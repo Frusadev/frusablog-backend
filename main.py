4f90d7486311a1211b49287a876ab2b0eba5d3ba
Frusadev <frusadev@gmail.com>
2024-10-10 18:00:05 +0000

    Add middlewares

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.api.endpoints.post import post_router
 from app.api.endpoints.user import user_router
 from app.api.endpoints.comment import comment_router
 from app.api.endpoints.auth.auth_handler import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 routers = [
     user_router,
@@ -12,7 +13,18 @@ routers = [
     auth_router
 ]
 
+origins = [
+    "http://localhost:3000"
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods="*",
+    allow_headers="*",
+)
 
 for router in routers:
     app.include_router(router)
